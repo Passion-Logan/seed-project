af20f8848a9ddee365fe4db2c89c4ee553d6e753
Passion-Logan <wangdadamailbox@163.com>
2020-06-16 11:01:53 +0800

    first commit

---
+++ src/test/java/com/cody/seed/SeedPeojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cody.seed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeedPeojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
